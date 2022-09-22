09f7bb8aadefcb839ad772cdf182615dbb680b25
devjo0810 <devjo0810@gmail.com>
2022-09-22 14:29:52 +0900

    드롭다운메뉴, 트리메뉴 추가

--- src/store/modules/Menu.js
+++ src/store/modules/Menu.js
@@ -1,55 +1,98 @@
 const sampleMenuList = [
   {
-    title: "Signup",
-    compoName: "Signup",
-  },
-  {
-    title: "Grid",
-    compoName: "Grid",
-  },
-  {
-    title: "Tree",
-    compoName: "Tree",
-  },
-  {
-    title: "Card",
-    compoName: "Card",
-  },
-  {
-    title: "TextEllipsisTitleTest_ABCDE",
-    compoName: "TextEllipsisTitleTest",
-    isOnlyOne: true,
-  },
-  {
     parentMenuId: "0",
     menuId: "1",
     sortSqnc: 1,
     title: "사용자 관리",
     compoName: null,
-    isMenu: false,
+    isWidget: false,
     menuList: [
       {
         parentMenuId: "1",
         menuId: "101",
-        title: "사용자 추가",
-        compoName: "AddUser",
-        isMenu: true,
+        title: "사용자 조회",
+        compoName: "SearchUser",
+        isWidget: true,
       },
       {
         parentMenuId: "1",
-        menuId: "101",
+        menuId: "102",
         title: "사용자 추가",
         compoName: "AddUser",
-        isMenu: true,
+        isWidget: true,
       },
       {
         parentMenuId: "1",
-        menuId: "101",
-        title: "사용자 추가",
-        compoName: "AddUser",
-        isMenu: true,
+        menuId: "103",
+        title: "사용자 제한",
+        compoName: "LimitUser",
+        menuList: [
+          {
+            parentMenuId: "103",
+            menuId: "10301",
+            title: "사용자 정지",
+            compoName: "StopUser",
+            isWidget: false,
+            menuList: [
+              {
+                parentMenuId: "1",
+                menuId: "101",
+                title: "사용자 조회",
+                compoName: "SearchUser",
+                isWidget: true,
+              },
+              {
+                parentMenuId: "1",
+                menuId: "101",
+                title: "사용자 조회",
+                compoName: "SearchUser",
+                isWidget: true,
+              },
+              {
+                parentMenuId: "1",
+                menuId: "101",
+                title: "사용자 조회",
+                compoName: "SearchUser",
+                isWidget: true,
+              },
+            ],
+          },
+          {
+            parentMenuId: "103",
+            menuId: "10302",
+            title: "사용자 메일전송",
+            compoName: "SendMailUser",
+            isWidget: true,
+          },
+        ],
       },
     ],
+  },
+  {
+    title: "Signup",
+    compoName: "Signup",
+    isWidget: true,
+  },
+  {
+    title: "Grid",
+    compoName: "Grid",
+    isWidget: true,
+  },
+  {
+    title: "Tree",
+    compoName: "Tree",
+    isWidget: true,
+  },
+  {
+    title: "Card",
+    compoName: "Card",
+    isWidget: true,
+  },
+  {
+    title: "TextEllipsisTitleTest_ABCDE",
+    compoName: "TextEllipsisTitleTest",
+    isWidget: true,
+    isOnlyOne: true,
   },
 ];
 
